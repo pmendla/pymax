d488de4b7f04a3f127454aa01840f5f1343c3047
pmendla <45833822+pmendla@users.noreply.github.com>
2018-12-12 23:18:25 +0100

    Fix connecting to Cube
    Closing socket explicitly in case of unsuccessful connection. Socket in original version stay open in SYN_SENT state after timeout exception preventing next connection to Cube.

--- src/pymax/cube.py
+++ src/pymax/cube.py
@@ -114,7 +114,11 @@ class Cube(object):
     def _create_socket(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(1)
-        s.connect(self.addr_port)
+        try:
+            s.connect(self.addr_port)
+        except:
+            s.close()
+            raise
         return s
 
     def disconnect(self):
